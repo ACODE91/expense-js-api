8c9c4d6b0508ef34476c4f1863896507d6f21855
alao <alao@marinsoftware.com>
2019-09-10 22:27:39 -0700

    Created Mandatory Columns constant

---
+++ constants/constants.js
@@ -0,0 +1,9 @@
+const MandatoryColumns = {
+  CUSTOMER_ID: "customerId",
+  EXPENSE: "expense",
+  PAYMENT: "payment",
+  DESCRIPTION: "description",
+  DATE: "date"
+};
+
+module.exports = MandatoryColumns;
