4888244fec7cd1d2778f67a86c328ceaf0f2564a
alao <alao@marinsoftware.com>
2019-09-10 22:26:26 -0700

    Created insertRow method
    Changes:
    - Created format columns to help join column arrays into a string
    - Created format values method to correctly format the string for querying into the database

--- dataservices/table-rows.dataservice.js
+++ dataservices/table-rows.dataservice.js
@@ -1,11 +1,40 @@
 const mySqlDatabase = require("../databases/expensedb.js");
+const _ = require("lodash");
+const MandatoryColumns = require("../constants/constants.js");
 
 class TableRowsDataService {
-  fetchRows() {
-    mySqlDatabase.query(`SELECT * FROM expensetable;`, function(err, result) {
+  async fetchRows() {
+    await mySqlDatabase.query(`SELECT * FROM expensetable;`, function(err, result) {
       if (err) throw err;
       return result;
     });
+  }
+
+  async insertRow({ customerId, expense, payment, description, date }) {
+    const columns = _.values(MandatoryColumns);
+    const values = _.values(arguments[0]);
+    const formattedColumns = this.formatColumns(columns);
+    const formattedValues = this.formatValues(values);
+    await mySqlDatabase.query(
+      `INSERT INTO expensetable (${formattedColumns}) VALUES(${formattedValues})`,
+      function(err, result) {
+        if (err) throw err;
+      }
+    );
+  }
+
+  formatColumns(inputArray) {
+    return inputArray.join(",");
+  }
+
+  formatValues(valuesArray) {
+    _.forEach(valuesArray, (value, index, array) => {
+      if (typeof value === "string") {
+        array[index] = `"${value}"`;
+      }
+    });
+
+    return valuesArray.join(",");
   }
 }
 
