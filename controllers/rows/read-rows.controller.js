93a88584f6a57a33acf1be33a36b1a5376f907b4
ACODE91 <aaron.z.lao@gmail.com>
2019-09-17 01:25:19 -0700

    Created read rows controller

--- controllers/rows/read-rows.controller.js
+++ controllers/rows/read-rows.controller.js
@@ -4,8 +4,10 @@ const TableRowsDataService = require("../../dataservices/table-rows.dataservice"
  * Fetch rows from expensedb.
  */
 function ReadRowsController(req, res) {
-  const rows = TableRowsDataService.fetchRows();
-  res.send(rows);
+  const rows = TableRowsDataService.fetchRows({ ...req.params });
+  rows.then((rowsData) => {
+    res.send(rowsData);
+  });
 }
 
 module.exports = {
